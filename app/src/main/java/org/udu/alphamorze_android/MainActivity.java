56d1dfabec4aa56348051f93fcb1599cb23c494d
undefuser <kleshnya26@gmail.com>
2017-11-26 21:57:03 +0700

    add main methods

--- app/src/main/java/org/udu/alphamorze_android/MainActivity.java
+++ app/src/main/java/org/udu/alphamorze_android/MainActivity.java
@@ -2,8 +2,17 @@ package org.udu.alphamorze_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSend;
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +20,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonSend = findViewById(R.id.button_send);
+        editText   = findViewById(R.id.enter_text_field);
+
+        char char_array[] ={'A', 'B', 'C', 'D', 'E', 'F',
+                            'G', 'H', 'I', 'J', 'K', 'L',
+                            'M', 'N', 'O', 'P', 'Q', 'R',
+                            'S', 'T', 'U', 'V', 'W', 'X',
+                                      'Y', 'Z'};
+
     }
+
+    private byte[] byteCodeFromText(final String text) { // Создание команд из 0 и 1
+
+        ArrayList<Byte> tempArrList = new ArrayList<>();
+
+        Byte[] res = tempArrList.toArray(new Byte[tempArrList.size()]);
+
+        return toPrimByte(res);
+
+    }
+
+    public void sendBinaryText(final View view) { // Обработчик нажатия на buttonSend
+
+        byte[] result_text;
+
+        result_text = byteCodeFromText(editText.getText().toString()).clone();
+
+    }
+
+    private byte[] toPrimByte(final Byte[] objByte) { // Конвертация объекта в примитив
+
+        byte[] result = new byte[objByte.length];
+
+        for (int e = 0; e < objByte.length; e++) {
+            result[e] = objByte[e];
+        }
+
+        return result;
+
+    }
+
+
 }
