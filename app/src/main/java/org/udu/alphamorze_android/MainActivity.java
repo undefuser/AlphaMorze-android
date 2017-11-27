1162fa20c3a9678965d72b08b55990693a434908
undefuser <kleshnya26@gmail.com>
2017-11-27 19:53:43 +0700

    finished work on methods

--- app/src/main/java/org/udu/alphamorze_android/MainActivity.java
+++ app/src/main/java/org/udu/alphamorze_android/MainActivity.java
@@ -23,19 +23,13 @@ public class MainActivity extends AppCompatActivity {
         buttonSend = findViewById(R.id.button_send);
         editText   = findViewById(R.id.enter_text_field);
 
-        char char_array[] ={'A', 'B', 'C', 'D', 'E', 'F',
-                            'G', 'H', 'I', 'J', 'K', 'L',
-                            'M', 'N', 'O', 'P', 'Q', 'R',
-                            'S', 'T', 'U', 'V', 'W', 'X',
-                                      'Y', 'Z'};
-
     }
 
     private byte[] byteCodeFromText(final String text) { // Создание команд из 0 и 1
 
         ArrayList<Byte> tempArrList = new ArrayList<>();
 
-
+        toCommand(text, tempArrList);
 
         Byte[] res = tempArrList.toArray(new Byte[tempArrList.size()]);
 
@@ -47,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         byte[] result_text;
 
-        result_text = byteCodeFromText(editText.getText().toString()).clone();
+        result_text = byteCodeFromText(editText.getText().toString().toUpperCase()).clone();
 
     }
 
@@ -63,13 +57,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void toCommand(final String text, ArrayList<Byte> arrayList) {
+    private void toCommand(final String text, ArrayList<Byte> arrayList) { // Преобразование текста в код
 
-
+        for (int e = 0; e < text.length(); e++) {
+            for (final byte temp : toCode(text.charAt(e))) {
+                arrayList.add(temp);
+            }
+        }
 
     }
 
-    private byte[] toCode(final char ch) {
+    private byte[] toCode(final char ch) { // Получение кода символа
 
         ArrayList<Byte> tempArrList = new ArrayList<>();
 
@@ -87,6 +85,194 @@ public class MainActivity extends AppCompatActivity {
             case 'C':
                 tempArrList.add((byte) 1);
                 tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'D':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case 'E':
+                tempArrList.add((byte) 0);
+                break;
+            case 'F':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'G':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'H':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case 'I':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case 'J':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case 'K':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case 'L':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case 'M':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case 'N':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'O':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case 'P':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'Q':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case 'R':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                break;
+            case 'S':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case 'T':
+                tempArrList.add((byte) 1);
+                break;
+            case 'U':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case 'V':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case 'W':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case 'X':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case 'Y':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case 'Z':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case '0':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case '1':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case '2':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case '3':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                break;
+            case '4':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 1);
+                break;
+            case '5':
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case '6':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case '7':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case '8':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 0);
+                tempArrList.add((byte) 0);
+                break;
+            case '9':
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
+                tempArrList.add((byte) 1);
                 tempArrList.add((byte) 1);
                 tempArrList.add((byte) 0);
                 break;
